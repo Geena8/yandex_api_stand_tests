4b6f47d183d4d827e55cb6b118cc386a4c2a4482
Geena8 <113479469+Geena8@users.noreply.github.com>
2024-02-24 03:11:54 +0300

    My first tester commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://20f0e20a-fd3c-4e60-bb50-9024739c9711.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
